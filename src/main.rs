0fe8e4503dd11c56239be2b3933311587f3520cd
Star-tears <1370443225@qq.com>
2022-11-03 22:36:07 +0800

    feat: 新增抛物线、立方体特效，更多颜色

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use std::{
     collections::BTreeSet,
     fs::File,
     io::{stdin, stdout, BufWriter, Write},
-    ops::IndexMut,
-    path::PathBuf,
     vec,
 };
 
@@ -53,7 +51,7 @@ struct Color {
 
 fn init_config() -> Config {
     print!("请输入相对y轴高度: ");
-    let path = r"H:\mc\mc粒子特效\midi_xlsx\最伟大的作品.xlsx";
+    let path = r"H:\mc\mc粒子特效\midi_xlsx\仙剑奇缘.xlsx";
     stdout().flush().unwrap();
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
@@ -61,9 +59,9 @@ fn init_config() -> Config {
     let y: i64 = s.next().unwrap().parse().unwrap();
     let config = Config {
         height: y,
-        tot_tick: 5000,
-        mid_pitch: 60,
-        midi_xlsx_path: r"H:\mc\mc粒子特效\midi_xlsx\最伟大的作品.xlsx".to_string(),
+        tot_tick: 1600,
+        mid_pitch: 56,
+        midi_xlsx_path: path.to_string(),
         output_setblocks_path: "./functions/setblocks.mcfunction".to_string(),
         output_play_path: "./functions/play.mcfunction".to_string(),
         output_clear_path: "./functions/clear.mcfunction".to_string(),
@@ -75,17 +73,17 @@ fn main() {
     let config = init_config();
 
     let mut workbook = Excel::open(config.midi_xlsx_path.as_str()).unwrap();
-    let sheet_list = workbook.sheet_names().unwrap();
-    println!("{}", sheet_list[0]);
+    // let sheet_list = workbook.sheet_names().unwrap();
+    // println!("{:#?}", sheet_list);
     // Read whole worksheet data and provide some statistics
-    let range = workbook.worksheet_range(sheet_list[0].as_str()).unwrap();
+    let range = workbook.worksheet_range("sheet1").unwrap();
     // let mut dest = BufWriter::new(File::create("./1.txt").unwrap());
     // write_range(&mut dest, &range);
     let tick_node_list: Vec<TickNode> = get_tick_node_list(&range);
     // println!("{:#?}", tick_node_list);
     let point_group_list = get_point_group_list(&tick_node_list, &config);
     create_setblocks_mcfunction(&point_group_list, &config);
-    // println!("{:#?}", point_group_list);
+    // println!("{:#?}", point_group_list[0]);
     let point_list = get_point_list(&tick_node_list, &config);
     // println!("{:?}", point_list);
     create_play_mcfunction(&point_group_list, &config);
@@ -155,29 +153,51 @@ fn get_point_group_list(tick_node_list: &Vec<TickNode>, config: &Config) -> Vec<
 fn get_sub_point_group_list(
     point_group_list: &Vec<PointGroup>,
     config: &Config,
+    mut group_count: usize,
+    mut sep_step: i64,
 ) -> Vec<Vec<PointGroup>> {
+    if group_count & 1 == 1 {
+        group_count += 2;
+        group_count -= group_count & 1;
+    }
+    if sep_step <= 0 {
+        sep_step = 5;
+    }
     let mut sub_point_group_list: Vec<Vec<PointGroup>> = Vec::new();
-    sub_point_group_list.push(Vec::new());
-    sub_point_group_list.push(Vec::new());
+    for i in 0..group_count {
+        sub_point_group_list.push(Vec::new());
+    }
+
     for point_group in point_group_list {
         let init_point_group = PointGroup {
             x: point_group.x,
             z_list: Vec::new(),
         };
-        sub_point_group_list[0].push(init_point_group.clone());
-        sub_point_group_list[1].push(init_point_group.clone());
+        for i in 0..group_count {
+            sub_point_group_list[i].push(init_point_group.clone());
+        }
         for (_, z) in point_group.z_list.iter().enumerate() {
-            if *z < config.mid_pitch {
-                sub_point_group_list[0].last_mut().unwrap().z_list.push(*z);
-            } else {
-                sub_point_group_list[1].last_mut().unwrap().z_list.push(*z);
+            let mut now_sep: i64 = config.mid_pitch - sep_step * (group_count as i64 / 2);
+            now_sep += sep_step;
+            for i in 0..group_count - 1 {
+                if *z <= now_sep {
+                    sub_point_group_list[i].last_mut().unwrap().z_list.push(*z);
+                    break;
+                } else if (i == group_count - 2) {
+                    sub_point_group_list[i + 1]
+                        .last_mut()
+                        .unwrap()
+                        .z_list
+                        .push(*z);
+                    break;
+                }
+                now_sep += sep_step;
             }
         }
-        if sub_point_group_list[0].last().unwrap().z_list.len() == 0 {
-            sub_point_group_list[0].pop();
-        }
-        if sub_point_group_list[1].last().unwrap().z_list.len() == 0 {
-            sub_point_group_list[1].pop();
+        for i in 0..group_count {
+            if sub_point_group_list[i].last().unwrap().z_list.len() == 0 {
+                sub_point_group_list[i].pop();
+            }
         }
     }
     sub_point_group_list
@@ -209,6 +229,7 @@ fn get_edge_list(point_group_list: &Vec<PointGroup>, config: &Config) -> Vec<Edg
         y: config.height,
         z: config.mid_pitch,
     };
+    let mut point_nxt: Point;
     for point_group in point_group_list {
         if point_group.z_list.len() > 1 {
             for i in 0..point_group.z_list.len() - 1 {
@@ -229,7 +250,7 @@ fn get_edge_list(point_group_list: &Vec<PointGroup>, config: &Config) -> Vec<Edg
             }
         }
         let index_nxt = rng.gen_range(0..point_group.z_list.len());
-        let point_nxt = Point {
+        point_nxt = Point {
             x: point_group.x,
             y: config.height,
             z: point_group.z_list[index_nxt],
@@ -240,6 +261,15 @@ fn get_edge_list(point_group_list: &Vec<PointGroup>, config: &Config) -> Vec<Edg
         });
         point_pre = point_nxt;
     }
+    point_nxt = Point {
+        x: config.tot_tick,
+        y: config.height,
+        z: config.mid_pitch,
+    };
+    edge_list.push(Edge {
+        point1: point_pre,
+        point2: point_nxt,
+    });
     edge_list
 }
 
@@ -265,58 +295,118 @@ fn create_play_mcfunction(point_group_list: &Vec<PointGroup>, config: &Config) {
     let mut rng = thread_rng();
     let mut dest = BufWriter::new(File::create(config.output_play_path.as_str()).unwrap());
     let color_list: Vec<Color> = vec![
+        //白色
         Color {
             r: 1.0,
             g: 1.0,
             b: 1.0,
             a: 1.0,
         },
+        //七彩色
         Color {
-            r: 1.0,
+            r: 0.8,
             g: 0.0,
             b: 0.0,
-            a: 1.0,
+            a: 0.9,
         },
         Color {
-            r: 1.0,
-            g: 0.644,
+            r: 0.8,
+            g: 0.544,
             b: 0.0,
-            a: 1.0,
+            a: 0.9,
         },
         Color {
-            r: 1.0,
-            g: 1.0,
+            r: 0.8,
+            g: 0.8,
             b: 0.0,
-            a: 1.0,
+            a: 0.9,
         },
         Color {
             r: 0.0,
-            g: 1.0,
+            g: 0.8,
             b: 0.0,
-            a: 1.0,
+            a: 0.9,
         },
         Color {
             r: 0.0,
-            g: 1.0,
-            b: 1.0,
-            a: 1.0,
+            g: 0.8,
+            b: 0.8,
+            a: 0.9,
         },
         Color {
             r: 0.0,
             g: 0.0,
-            b: 1.0,
-            a: 1.0,
+            b: 0.8,
+            a: 0.9,
         },
         Color {
-            r: 0.542,
+            r: 0.442,
             g: 0.0,
-            b: 1.0,
-            a: 1.0,
+            b: 0.8,
+            a: 0.9,
+        },
+        // 亮rgb
+        Color {
+            r: 0.95,
+            g: 0.45,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.95,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.45,
+            b: 0.95,
+            a: 0.9,
+        },
+        //渐变基色
+        Color {
+            r: 0.65,
+            g: 0.55,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.65,
+            g: 0.45,
+            b: 0.55,
+            a: 0.9,
+        },
+        Color {
+            r: 0.55,
+            g: 0.65,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.55,
+            g: 0.45,
+            b: 0.65,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.65,
+            b: 0.55,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.55,
+            b: 0.65,
+            a: 0.9,
         },
     ];
     let sub_point_group_list: Vec<Vec<PointGroup>> =
-        get_sub_point_group_list(&point_group_list, config);
+        get_sub_point_group_list(&point_group_list, config, 4, 10);
+    let mut count = 0;
     for sub_point_group in sub_point_group_list {
+        count += 1;
         let edge_list = get_edge_list(&sub_point_group, config);
         for edge in edge_list {
             if edge.point1.x == edge.point2.x {
@@ -327,17 +417,27 @@ fn create_play_mcfunction(point_group_list: &Vec<PointGroup>, config: &Config) {
                     color_list.get(0).unwrap(),
                 );
             } else {
-                let index_color = rng.gen_range(1..8);
-                draw_line(
-                    &mut dest,
-                    edge.point1,
-                    edge.point2,
-                    color_list.get(index_color).unwrap(),
-                );
+                let index_color = rng.gen_range(11..17);
+                if count & 1 == 1 {
+                    draw_parabola(
+                        &mut dest,
+                        edge.point1,
+                        edge.point2,
+                        color_list.get(index_color).unwrap(),
+                    );
+                } else {
+                    draw_line(
+                        &mut dest,
+                        edge.point1,
+                        edge.point2,
+                        color_list.get(index_color).unwrap(),
+                    );
+                }
             }
         }
-        create_tp_mcfuntion(&mut dest, config);
     }
+    create_rhythm_point_mcfunction(&mut dest, config, point_group_list);
+    create_tp_mcfuntion(&mut dest, config);
 }
 
 fn create_clear_mcfunction(tick_node_list: &Vec<TickNode>, config: &Config) {
@@ -369,6 +469,57 @@ fn create_tp_mcfuntion(dest: &mut dyn Write, config: &Config) {
     }
 }
 
+fn create_rhythm_point_mcfunction(
+    dest: &mut dyn Write,
+    config: &Config,
+    point_group_list: &Vec<PointGroup>,
+) {
+    let mut rng = thread_rng();
+    let color_list: Vec<Color> = vec![
+        //白色
+        Color {
+            r: 1.0,
+            g: 1.0,
+            b: 1.0,
+            a: 1.0,
+        },
+        // 亮rgb
+        Color {
+            r: 0.95,
+            g: 0.45,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.95,
+            b: 0.45,
+            a: 0.9,
+        },
+        Color {
+            r: 0.45,
+            g: 0.45,
+            b: 0.95,
+            a: 0.9,
+        },
+    ];
+    for point_group in point_group_list {
+        for (_, z) in point_group.z_list.iter().enumerate() {
+            let index_color = rng.gen_range(0..4);
+            let point = Point {
+                x: point_group.x,
+                y: config.height,
+                z: *z,
+            };
+            draw_cube(dest, point, color_list.get(index_color).unwrap());
+        }
+    }
+}
+
+fn draw_cube(dest: &mut dyn Write, point: Point, color: &Color) {
+    write!(dest,"execute if score @p Timer matches {} run particleex conditional minecraft:end_rod ~{} ~{} ~{} {:.3} {:.3} {:.3} {:.3} 0 0.4 0 0.5 0.5 0.5 \"dis>0.7\" 0.1 60\r\n",point.x,point.x,point.y,point.z,color.r,color.g,color.b,color.a).unwrap();
+}
+
 fn draw_line(dest: &mut dyn Write, point1: Point, point2: Point, color: &Color) {
     let off_x = point2.x - point1.x;
     let off_y = point2.y - point1.y;
@@ -391,9 +542,20 @@ fn draw_line(dest: &mut dyn Write, point1: Point, point2: Point, color: &Color) 
     }
     let num_sep: f64 = 1.0 / (num_cpt as f64);
     if off_x != 0 {
-        write!(dest,"execute if score @p Timer matches {} run particleex tickparameter minecraft:end_rod ~{} ~{} ~{} {:.3} {:.3} {:.3} {:.3} 0 0 0 0 {} \"x={}*t;y={:.5}*t;z={:.5}*t;\" {:.5} {} 25\r\n",point1.x,point1.x,point1.y,point1.z,color.r,color.g,color.b,color.a,off_x,k_x,k_y,k_z,num_sep,num_cpt).unwrap();
+        write!(dest,"execute if score @p Timer matches {} run particleex rgbatickparameter minecraft:end_rod ~{} ~{} ~{} 0 0 0 0 {} \"x,y,z,cr,cg,cb={}*t,{:.5}*t,{:.5}*t,sin(t/7)/8+{},sin(t/5)/8+{},sin(t/3)/8+{}\" {} {} 25\r\n",point1.x,point1.x,point1.y,point1.z,off_x,k_x,k_y,k_z,color.r,color.g,color.b,num_sep,num_cpt).unwrap();
     } else {
-        write!(dest,"execute if score @p Timer matches {} run particleex parameter minecraft:end_rod ~{} ~{} ~{} {:.3} {:.3} {:.3} {:.3} 0 0 0 0 {:.5} \"x={}*t;y={:.5}*t;z={:.5}*t;\" {:.5} 25\r\n",point1.x,point1.x,point1.y,point1.z,color.r,color.g,color.b,color.a,dist,k_x,k_y,k_z,num_sep).unwrap();
+        write!(dest,"execute if score @p Timer matches {} run particleex parameter minecraft:end_rod ~{} ~{} ~{} {:.3} {:.3} {:.3} {:.3} 0 0 0 0 {:.5} \"x={}*t;y={:.5}*t;z={:.5}*t;\" {} 25\r\n",point1.x,point1.x,point1.y,point1.z,color.r,color.g,color.b,color.a,dist,k_x,k_y,k_z,num_sep).unwrap();
+    }
+}
+
+fn draw_parabola(dest: &mut dyn Write, point1: Point, point2: Point, color: &Color) {
+    let off_x = point2.x - point1.x;
+    let off_y = point2.y - point1.y;
+    let off_z = point2.z - point1.z;
+    let k_y2 = -0.1;
+    let k_z: f64 = (off_z as f64) / (off_x as f64);
+    if off_x != 0 {
+        write!(dest,"execute if score @p Timer matches {} run particleex rgbatickparameter minecraft:end_rod ~{} ~{} ~{} 0 0 0 0 {} \"x,y,z,cr,cg,cb=t,{:.3}*t*(t-{}),{:.3}*t,sin(t/7)/8+{},sin(t/5)/8+{},sin(t/3)/8+{}\" 0.05 20 25\r\n",point1.x,point1.x,point1.y,point1.z,off_x,k_y2,off_x,k_z,color.r,color.g,color.b).unwrap();
     }
 }
 
